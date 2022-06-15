cc5a54280b52708f46b947fee5d882fa28ef45be
plusdojo2022ishida <ishida-kaho-plusdojo2022@seplus2016.onmicrosoft.com>
2022-06-15 11:56:46 +0900

    Follow/MasterUserのJavaBeans

--- src/src/model/Follow.java
+++ src/src/model/Follow.java
@@ -1,5 +1,54 @@
 package model;
 
-public class Follow {
+import java.io.Serializable;
 
+public class Follow implements Serializable{
+	private String id;				//ID
+	private String user_id;			//ユーザーID
+	private String follow_id;		//フォローID
+
+	//引数があるコンストラクタ
+	public Follow(String id, String user_id, String follow_id) {
+		super();
+		this.id = id;
+		this.user_id = user_id;
+		this.follow_id = follow_id;
+	}
+
+	//引数がないコンストラクタ
+	public Follow() {
+		super();
+		this.id = "";
+		this.user_id = "";
+		this.follow_id = "";
+	}
+
+	//getter/setter
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+
+	public String getFollow_id() {
+		return follow_id;
+	}
+
+	public void setFollow_id(String follow_id) {
+		this.follow_id = follow_id;
+	}
 }
+
+
+
+
