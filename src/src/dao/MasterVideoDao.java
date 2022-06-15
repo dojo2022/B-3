63481064b992d32da7cb2ca6624475113882f3fb
dojo2022-Endo <103239304+dojo2022-Endo@users.noreply.github.com>
2022-06-15 17:16:55 +0900

    h

--- src/src/dao/MasterVideoDao.java
+++ src/src/dao/MasterVideoDao.java
@@ -1,5 +1,95 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.MasterVideo;
+
 public class MasterVideoDao {
 
+	public List<MasterVideo> select(MasterVideo param) {
+		Connection conn = null;
+		List<MasterVideo> cardList = new ArrayList<MasterVideo>();
+
+		try {
+			// JDBCドライバを読み込む
+			Class.forName("org.h2.Driver");
+
+			// データベースに接続する
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6Data/dojo6Data", "sa", "");
+
+			// SQL文を準備する
+			String sql = "SELECT video_id, video_name, video_year, video_time, genre_id from MasterVideo WHERE video_name LIKE ? AND video_year LIKE ? AND video_time LIKE ? AND genre_id LIKE ? ORDER BY NUMBER";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+
+			// SQL文を完成させる
+			if (param.getVideo_name() != null) {
+				pStmt.setString(1, "%" + param.getVideo_name() + "%");
+			}
+			else {
+				pStmt.setString(1, "%");
+			}
+			if (param.getVideo_year() != null) {
+				pStmt.setString(2, "%" + param.getVideo_year() + "%");
+			}
+			else {
+				pStmt.setString(2, "%");
+			}
+			if (param.getVideo_time() != null) {
+				pStmt.setString(3, "%" + param.getVideo_time() + "%");
+			}
+			else {
+				pStmt.setString(3, "%");
+			}
+			if (param.getGenre_id() != null) {
+				pStmt.setString(4, "%" + param.getGenre_id() + "%");
+			}
+			else {
+				pStmt.setString(4, "%");
+			}
+
+			// SQL文を実行し、結果表を取得する
+			ResultSet rs = pStmt.executeQuery();
+
+			// 結果表をコレクションにコピーする
+			while (rs.next()) {
+				MasterVideo card = new MasterVideo(
+				rs.getString("video_id"),
+				rs.getString("video_name"),
+				rs.getString("video_year"),
+				rs.getString("video_time"),
+				rs.getString("genre_id")
+				);
+				cardList.add(card);
+			}
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+			cardList = null;
+		}
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			cardList = null;
+		}
+		finally {
+			// データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				}
+				catch (SQLException e) {
+					e.printStackTrace();
+					cardList = null;
+				}
+			}
+		}
+
+		// 結果を返す
+		return cardList;
+	}
 }
