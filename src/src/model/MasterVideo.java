b0057ec8c1add80aac426885ae340ed08f803765
dojo2022-takayama <103238629+dojo2022-takayama@users.noreply.github.com>
2022-06-15 13:44:06 +0900

    Update MasterVideo.java

--- src/src/model/MasterVideo.java
+++ src/src/model/MasterVideo.java
@@ -1,5 +1,52 @@
 package model;
 
 public class MasterVideo {
+	private String id;
+	private String name;
+	private int year;
+	private int time;
+	private String genreID;
 
+	public MasterVideo(String id, String name, int year, int time, String genreID) {
+		this.id = id;
+		this.name = name;
+		this.year = year;
+		this.time = time;
+		this.genreID = genreID;
+	}
+
+	public MasterVideo() {
+		this(null, null, -1, -1, null);
+	}
+
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	public int getTime() {
+		return time;
+	}
+	public void setTime(int time) {
+		this.time = time;
+	}
+	public String getGenreID() {
+		return genreID;
+	}
+	public void setGenreID(String genreID) {
+		this.genreID = genreID;
+	}
 }
