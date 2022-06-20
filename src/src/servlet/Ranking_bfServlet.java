fe609fe5f2ea293515951e3356a6e888e42c1484
plusdojo2022-ito <103239307+plusdojo2022-ito@users.noreply.github.com>
2022-06-20 17:42:44 +0900

    ranking

--- src/src/servlet/Ranking_bfServlet.java
+++ src/src/servlet/Ranking_bfServlet.java
@@ -12,6 +12,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import dao.RankingDAO;
+import model.Ranking;
+
 /**
  * Servlet implementation class Ranking_bfServlet
  */
@@ -21,10 +24,48 @@ public class Ranking_bfServlet extends HttpServlet {
 
 			protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 				Map< String, List<String> > map = new HashMap< String, List<String> >();
-				request.setAttribute("Genre", map);
+				request.setAttribute("ReviewRanking", map);
+				//通算別ランキングを取得する
+				RankingDAO rankDao = new RankingDAO();
+				List<Ranking> totalRank=rankDao.selectAll();
+				//結果をリクエストスコープに入れる
+				request.setAttribute("totalRank", totalRank);
+
+				//国内別ランキングを取得する
+				List<Ranking> localRank=rankDao.selectLocal();
+				//結果をリクエストスコープに入れる
+				request.setAttribute("localRank",localRank);
+
+
+				//アニメ別ランキングを取得する
+				List<Ranking> animeRank=rankDao.selectAnime();
+				//結果をリクエストスコープに入れる
+				request.setAttribute("animeRank",animeRank);
+
+
 				//ランキングページへ遷移
 				RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/ranking_bf.jsp");
 				dispatcher.forward(request, response);
-	}
 
+	}
+			protected void doPost(HttpServletRequest request, HttpServletResponse response)
+					throws ServletException, IOException {
+				// リクエストパラメータを取得する
+				request.setCharacterEncoding("UTF-8");
+				String video_id = request.getParameter("video_id");
+				String video_name = request.getParameter("video_name");
+
+				// 検索処理を行う
+				RankingDAO rDao = new RankingDAO();
+				List<Ranking> ReviewRanking = rDao.select(new Ranking(video_id, video_name));
+
+				// 検索結果をリクエストスコープに格納する
+				request.setAttribute("ReviewRanking", ReviewRanking);
+
+				//ランキングページへ遷移
+				RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/ranking_bf.jsp");
+				dispatcher.forward(request, response);
+
+
+	}
 }
