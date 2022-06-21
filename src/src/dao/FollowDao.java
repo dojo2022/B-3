fd4d68f96e555ed8f4cb5959c2040716ae48f56e
plusdojo2022ishida <ishida-kaho-plusdojo2022@seplus2016.onmicrosoft.com>
2022-06-21 09:37:30 +0900

    Update FollowDao.java

--- src/src/dao/FollowDao.java
+++ src/src/dao/FollowDao.java
@@ -36,7 +36,6 @@ public class FollowDao {
 					+ "AND t_follow.user_id = ?";
 			PreparedStatement pStmt = conn.prepareStatement(FollowUser);
 
-
 			// SQL文を完成させる　？に入る言葉(paramに入る言葉)をデータベースから取り出す
 			// 1，2，3は何番目の'？'か
 			pStmt.setString(1, user_id);
@@ -79,6 +78,110 @@ public class FollowDao {
 		}
 		return followList;
 	}
+//フォローの人数を返すメソッドfollowcount
+	public int Followcount(String user_id) {
+
+		Connection conn = null;
+		int count=0;
+		try {
+			// JDBCドライバを読み込む
+			Class.forName("org.h2.Driver");
+
+			// データベースに接続する(変更済み）
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6Data/dojo6Data", "sa", "");
+
+			// SQL文を準備する(pStmtの意味は、コンパイルされた(実行可能な)SQL文)
+			// SQLを使いたいときは必ずpStmtをつけると思っといていい!
+			//フォロー一覧
+			String FollowUser = "SELECT count(id) as counts FROM t_follow  WHERE user_id = ? ";
+
+			PreparedStatement pStmt = conn.prepareStatement(FollowUser);
+
+
+			// SQL文を完成させる　？に入る言葉をデータベースから取り出す
+			// 1，2，3は何番目の'？'か
+			pStmt.setString(1, user_id);
+
+			// SQL文を実行し、結果表を取得する(変更なし)
+			ResultSet rs = pStmt.executeQuery();
+
+			// 結果表(rs)をコレクションにコピーする
+			rs.next();
+			count = rs.getInt("counts");
+			}
+
+		//e は例外exceptionの"e"
+		catch (SQLException e) {
+			e.printStackTrace();
+			count = 0;
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			count = 0;
+		} finally {
+			// データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+					count = 0;
+				}
+			}
+		}
+		return count;
+	}
+	//フォロワーの人数を返すメソッドfollowercount
+		public int Followercount(String user_id) {
+
+			Connection conn = null;
+			int count=0;
+			try {
+				// JDBCドライバを読み込む
+				Class.forName("org.h2.Driver");
+
+				// データベースに接続する(変更済み）
+				conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6Data/dojo6Data", "sa", "");
+
+				// SQL文を準備する(pStmtの意味は、コンパイルされた(実行可能な)SQL文)
+				// SQLを使いたいときは必ずpStmtをつけると思っといていい!
+				//フォロー一覧
+				String FollowUser = "SELECT count(id) as counts FROM t_follow  WHERE follow_id = ? ";
+
+				PreparedStatement pStmt = conn.prepareStatement(FollowUser);
+
+
+				// SQL文を完成させる　？に入る言葉をデータベースから取り出す
+				// 1，2，3は何番目の'？'か
+				pStmt.setString(1, user_id);
+
+				// SQL文を実行し、結果表を取得する(変更なし)
+				ResultSet rs = pStmt.executeQuery();
+
+				// 結果表(rs)をコレクションにコピーする
+				rs.next();
+				count = rs.getInt("counts");
+				}
+
+			//e は例外exceptionの"e"
+			catch (SQLException e) {
+				e.printStackTrace();
+				count = 0;
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+				count = 0;
+			} finally {
+				// データベースを切断
+				if (conn != null) {
+					try {
+						conn.close();
+					} catch (SQLException e) {
+						e.printStackTrace();
+						count = 0;
+					}
+				}
+			}
+			return count;
+		}
 
 //フォロワー一覧を返すメソッドFollowerUser
 	//user_idが引数
