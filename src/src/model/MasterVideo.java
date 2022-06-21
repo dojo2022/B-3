863b22c3fef418250cce9dbf01214a19887a769c
dojo2022-Endo <103239304+dojo2022-Endo@users.noreply.github.com>
2022-06-21 16:34:00 +0900

    s

--- src/src/model/MasterVideo.java
+++ src/src/model/MasterVideo.java
@@ -4,21 +4,12 @@ import java.io.Serializable;
 public class MasterVideo implements Serializable{
 	private String video_id;	    // 作品ID
 	private String video_name;		// 作品名
-<<<<<<< HEAD
-	private Int video_year;      // 年
-	private Int video_time;  	// 再生時間
-	private String genre_id;		// ジャンルID
-
-	// 引数があるコンストラクタ
-	public MasterVideo(String video_id, String video_name, Int video_year, Int video_time, String genre_id) {
-=======
 	private int    video_year;      // 年
 	private int    video_time;  	// 再生時間
 	private String genre_id;		// ジャンルID
 
 	// 引数があるコンストラクタ
 	public MasterVideo(String video_id, String video_name, int video_year, int video_time, String genre_id) {
->>>>>>> a0b0b8f9179b0e920c056896b17005192a4572a2
 		super();
 		this.video_id = video_id;
 		this.video_name = video_name;
@@ -54,21 +45,6 @@ public class MasterVideo implements Serializable{
 		this.video_name = video_name;
 	}
 
-<<<<<<< HEAD
-	public Int getVideo_year() {
-		return video_year;
-	}
-
-	public void setVideo_year(Int video_year) {
-		this.video_year = video_year;
-	}
-
-	public Int getVideo_time() {
-		return video_time;
-	}
-
-	public void setVideo_time(Int video_time) {
-=======
 	public int getVideo_year() {
 		return video_year;
 	}
@@ -82,7 +58,6 @@ public class MasterVideo implements Serializable{
 	}
 
 	public void setVideo_time(int video_time) {
->>>>>>> a0b0b8f9179b0e920c056896b17005192a4572a2
 		this.video_time = video_time;
 	}
 
