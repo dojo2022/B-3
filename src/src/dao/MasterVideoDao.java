132a430868787af5e3380d32e954a4e4e5227aed
dojo2022-takayama <103238629+dojo2022-takayama@users.noreply.github.com>
2022-06-22 09:42:20 +0900

    感想カテゴリを使った検索機能の追加

--- src/src/dao/MasterVideoDao.java
+++ src/src/dao/MasterVideoDao.java
@@ -176,44 +176,115 @@ public class MasterVideoDao {
 	}
 
 	public List<MasterVideo> selectFromReview(MasterVideo params, String feelcat1, String feelcat2) {
-		return select(params);
-		// Connection conn = null;
-		// List<MasterVideo> videoList = new ArrayList<MasterVideo>();
 
-		// try {
-		// 	// JDBCドライバを読み込む
-		// 	Class.forName("org.h2.Driver");
+		Connection conn = null;
+		List<MasterVideo> videoList = new ArrayList<MasterVideo>();
 
-		// 	// データベースに接続する
-		// 	conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6Data/dojo6Data", "sa", "");
+		try {
+			// JDBCドライバを読み込む
+			Class.forName("org.h2.Driver");
 
-		// 	// SQL文を準備する
-		// 	String sql = " SELECT *"
-		// 			   + " FROM t_review"
-		// 			   ;
-		// 	PreparedStatement pStmt = conn.prepareStatement(sql);
+			// データベースに接続する
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6Data/dojo6Data", "sa", "");
 
-		// }
-		// catch (SQLException e) {
-		// 	e.printStackTrace();
-		// 	videoList = null;
-		// }
-		// catch (ClassNotFoundException e) {
-		// 	e.printStackTrace();
-		// 	videoList = null;
-		// }
-		// finally {
-		// 	// データベースを切断
-		// 	if (conn != null) {
-		// 		try {
-		// 			conn.close();
-		// 		}
-		// 		catch (SQLException e) {
-		// 			e.printStackTrace();
-		// 			videoList = null;
-		// 		}
-		// 	}
-		// }
-		// return null;
+			// SQL文を準備する
+			String sql = " SELECT V.*"
+					   + " FROM t_review as R, m_video as V"
+					   + " WHERE R.video_id = V.video_id"
+					   + " AND R.feelcat_name1 LIKE ?"
+					   + " AND R.feelcat_name2 LIKE ?"
+					   + " AND V.video_name LIKE ?"
+					   + " AND V.video_year BETWEEN ? AND ?"
+					   + " AND V.video_time BETWEEN ? AND ?"
+					   + " AND V.genre_id LIKE ?"
+					   + " GROUP BY R.video_id"
+					   ;
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+
+			if (feelcat1 != null && !feelcat1.isEmpty()) {
+				pStmt.setString(1, "%" + feelcat1 + "%");
+			} else {
+				pStmt.setString(1, "%");
+			}
+
+			if (feelcat2 != null && !feelcat2.isEmpty()) {
+				pStmt.setString(2, "%" + feelcat2 + "%");
+			} else {
+				pStmt.setString(2, "%");
+			}
+
+			if (params.getVideo_name() != null && !params.getVideo_name().isEmpty()) {
+				pStmt.setString(3, "%" + params.getVideo_name() + "%");
+			}
+			else {
+				pStmt.setString(3, "%");
+			}
+
+			int year = params.getVideo_year();
+			if (year > 1970) {
+				pStmt.setInt(4, year - 10);
+				pStmt.setInt(5, year - 1);
+			} else if (year == 1970) {
+				pStmt.setInt(4, 0);
+				pStmt.setInt(5, year - 1);
+			} else {
+				pStmt.setInt(4, 0);
+				pStmt.setInt(5, 3000);
+			}
+
+			int time = params.getVideo_time();
+			if (time > 180) {
+				pStmt.setInt(6, 181);
+				pStmt.setInt(7, 1440);
+			} else if (time >= 30) {
+				pStmt.setInt(6, time - 30 + 1);
+				pStmt.setInt(7, time);
+			} else {
+				pStmt.setInt(6, 0);
+				pStmt.setInt(7, 1440);
+			}
+			if (params.getGenre_id() != null) {
+				pStmt.setString(8, params.getGenre_id());
+			}
+			else {
+				pStmt.setString(8, "%");
+			}
+
+			// SQL文を実行し、結果表を取得する
+			ResultSet rs = pStmt.executeQuery();
+
+			// 結果表をコレクションにコピーする
+			while (rs.next()) {
+				MasterVideo video = new MasterVideo(
+				rs.getString("video_id"),
+				rs.getString("video_name"),
+				rs.getInt("video_year"),
+				rs.getInt("video_time"),
+				rs.getString("genre_id")
+				);
+				videoList.add(video);
+			}
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+			videoList = null;
+		}
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			videoList = null;
+		}
+		finally {
+			// データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				}
+				catch (SQLException e) {
+					e.printStackTrace();
+					videoList = null;
+				}
+			}
+		}
+		return videoList;
 	}
 }
