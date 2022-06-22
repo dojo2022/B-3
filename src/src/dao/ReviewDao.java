539c28ee63f7bec3520199823826244f555bd23a
dojo2022-hashimoto <hashimoto-miku-plusdojo2022@seplus2016.onmicrosoft.com>
2022-06-22 10:45:41 +0900

    ReviewDAO マイページレビュー一覧表示追加

--- src/src/dao/ReviewDao.java
+++ src/src/dao/ReviewDao.java
@@ -476,6 +476,67 @@ public class ReviewDao {
 				return Reviewlist;
 			}
 
+			//マイページレビュー一覧表示
+			public List<Reviewdata> select2() {
+				Connection conn = null;
+				List<Reviewdata> Reviewdata = new ArrayList<Reviewdata>();
+
+				try {
+					// JDBCドライバを読み込む
+					Class.forName("org.h2.Driver");
+
+					// データベースに接続する
+					conn = DriverManager.getConnection("jdbc:h2:file:C:/Dojo6Data/dojo6", "sa", "");
+
+					// SQL文を準備する
+
+					String sql = "SELECT m_user.user_name,m_user.user_img,m_video.video_name,t_review.star,t_review.review_date,m_genre.genre_name,t_review.feelcat_name1,t_review.feelcat_name2,t_review.review_contents FROM t_review  LEFT JOIN m_user ON t_review.user_id = m_user.user_id LEFT JOIN m_video ON t_review.video_id = m_video.video_id LEFT JOIN m_genre ON t_review.genre_id = m_genre.genre_id WHERE \r\n"
+							+ "m_user.user_id = ?";
+					PreparedStatement pStmt = conn.prepareStatement(sql);
+
+					// SQL文を実行し、結果表を取得する
+					ResultSet rs = pStmt.executeQuery();
+
+					// 結果表をコレクションにコピーする
+					while (rs.next()) {
+						Reviewdata card = new Reviewdata(
+						rs.getString("user_name"),
+						rs.getString("user_img"),
+						rs.getString("video_name"),
+						rs.getString("star"),
+						rs.getString("review_date"),
+						rs.getString("genre_name"),
+						rs.getString("feelcat_name1"),
+						rs.getString("feelcat_name2"),
+						rs.getString("review_contents")
+						);
+						Reviewdata.add(card);
+					}
+				}
+				catch (SQLException e) {
+					e.printStackTrace();
+					Reviewdata = null;
+				}
+				catch (ClassNotFoundException e) {
+					e.printStackTrace();
+					Reviewdata = null;
+				}
+				finally {
+					// データベースを切断
+					if (conn != null) {
+						try {
+							conn.close();
+						}
+						catch (SQLException e) {
+							e.printStackTrace();
+							Reviewdata = null;
+						}
+					}
+				}
+
+				// 結果を返す
+				return Reviewdata;
+			}
 
 
 		}
